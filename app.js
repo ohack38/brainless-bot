5139c3ce8f903138e6355896bb459433f18a72d1
Oscar Hackman <ohackman1@gmail.com>
2021-10-29 16:54:02 +0300

    serverless setup, buildspec

--- app.js
+++ app.js
@@ -1,14 +1,64 @@
-const { App } = require("@slack/bolt");
+const { App, AwsLambdaReceiver } = require("@slack/bolt");
 require("dotenv").config();
 // Initializes your app with your bot token and signing secret
+const awsLambdaReceiver = new AwsLambdaReceiver({
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
+
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
-  socketMode:true,
+  receiver: awsLambdaReceiver,
+  processBeforeResponse: true,
+  //socketMode:true,
   appToken: process.env.APP_TOKEN
 });
 
-app.command("/test", async ({ command, ack, say }) => {
+
+// Listens to incoming messages that contain "hello"
+app.message('hello', async ({ message, say }) => {
+    // say() sends a message to the channel where the event was triggered
+    await say({
+      blocks: [
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": `Hey there <@${message.user}>!`
+          },
+          "accessory": {
+            "type": "button",
+            "text": {
+              "type": "plain_text",
+              "text": "Click Me"
+            },
+            "action_id": "button_click"
+          }
+        }
+      ],
+      text: `Hey there <@${message.user}>!`
+    });
+  });
+  
+// Listens for an action from a button click
+app.action('button_click', async ({ body, ack, say }) => {
+  await say(`<@${body.user.id}> clicked the button`);
+  
+  // Acknowledge the action after say() to exit the Lambda process
+  await ack();
+});
+  
+// Listens to incoming messages that contain "goodbye"
+app.message('goodbye', async ({ message, say }) => {
+    await say(`See ya later, <@${message.user}> :wave:`);
+});
+  
+  // Handle the Lambda function event
+module.exports.handler = async (event, context, callback) => {
+    const handler = await awsLambdaReceiver.start();
+    return handler(event, context, callback);
+}
+
+/* app.command("/test", async ({ command, ack, say }) => {
     try {
       await ack();
       say("Yaaay! that command works!");
@@ -23,4 +73,4 @@ app.command("/test", async ({ command, ack, say }) => {
     // Start your app
     await app.start(process.env.PORT || port);
     console.log(`⚡️ Slack Bolt app is running on port ${port}!`);
-  })();
+  })(); */
