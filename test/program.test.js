3713a7b029fff8d0c2853dc2cef0fccfb61e8696
karellje <karellje@arcada.fi>
2021-10-31 15:37:32 +0200

    jest.js

---
+++ test/program.test.js
@@ -0,0 +1,55 @@
+
+class Stack{
+    constructor(){
+        this.top = -1;
+        this.items = {};
+    }
+    get peek(){
+        return this.items[this.top];
+    }
+    push(value){
+        this.top +=1;
+        this.items[this.top] = value;
+    }
+    get remove(){
+        return this.items[-1];
+    }
+}
+
+
+
+describe('My Stack',()=> {
+
+    let stack;
+    beforeEach(()=>{
+        stack = new Stack;
+    })
+
+    it('is created empty',()=> {
+
+        expect(stack.top).toBe(-1);
+        expect(stack.items).toEqual({});
+    });
+    it('can push to the top',()=> {
+
+        stack.push('banan');
+
+        expect(stack.top).toBe(0);
+        expect(stack.peek).toBe("banan");
+    });
+
+    it('can pop off',()=> {
+        stack.push('banan');
+        stack.remove;
+        expect(stack.peek).toEqual(-1);
+
+        //stack.remove;
+
+        //expect(stack.top).toBe(-1);
+        //expect(stack.peek).toBe("banan");
+    });
+
+
+
+
+})
